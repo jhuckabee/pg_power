4316b8b60e10ec721dcc38402d2df94f5555c15c
Josh Huckabee <joshhuckabee@gmail.com>
2022-06-09 18:47:37 -0400

    Upgrade to be compatible with pg 1.3.5

--- lib/pg_power/connection_adapters/postgresql_adapter/translate_exception.rb
+++ lib/pg_power/connection_adapters/postgresql_adapter/translate_exception.rb
@@ -4,11 +4,11 @@ module PgPower::ConnectionAdapters::PostgreSQLAdapter::TranslateException
   # # See http://www.postgresql.org/docs/9.1/static/errcodes-appendix.html
   INSUFFICIENT_PRIVILEGE = "42501"
 
-  # Intercept insufficient privilege PGError and raise active_record wrapped database exception
+  # Intercept insufficient privilege PG::Error and raise active_record wrapped database exception
   def translate_exception(exception, message)
-    case exception.result.try(:error_field, PGresult::PG_DIAG_SQLSTATE)
+    case exception.result.try(:error_field, PG:Result::PG_DIAG_SQLSTATE)
       when INSUFFICIENT_PRIVILEGE
-        exc_message = exception.result.try(:error_field, PGresult::PG_DIAG_MESSAGE_PRIMARY)
+        exc_message = exception.result.try(:error_field, PG::Result::PG_DIAG_MESSAGE_PRIMARY)
         exc_message ||= message
         ::ActiveRecord::InsufficientPrivilege.new(exc_message, exception)
       else
